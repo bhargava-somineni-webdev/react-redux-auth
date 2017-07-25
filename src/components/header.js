d7bed6b7a9ee17d7e57db61734e9405c8aac0143
bhargava-somineni <somineni3@gmail.com>
2017-07-25 18:06:26 -0400

    Add action creator and reducer to handle login submit and dispatch action on successful login

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light">
-                <ul className="nav navbar-nav">
-                    <li className="nav-item">
-                        Sign in
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+         <ul className="nav navbar-nav">
+        </ul>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+
+export default connect(null)(Header);
--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
 import Signin from './components/auth/signin';
 import reducers from './reducers';
+import reduxThunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
